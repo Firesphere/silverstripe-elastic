3b2bfe05eba831d3bc2bb062c10cf77759af8a17
Simon Erkelens <simon@pikselin.com>
2023-09-29 11:58:18 +1300

    Add initial composer etc.

---
+++ src/Models/SearchSynonym.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Firesphere\ElasticSearch\Models;
+
+use SilverStripe\ORM\DataObject;
+
+class SearchSynonym extends DataObject
+{
+
+}
---
+++ src/Services/ElasticService.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Firesphere\ElasticSearch\Services;
+
+class ElasticService
+{
+
+}
