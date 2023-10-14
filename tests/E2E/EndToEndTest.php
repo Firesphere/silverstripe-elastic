73b3d85ab1949ab0f6efcb85c18c2e5b4f27ee8b
Simon Erkelens <github@casa-laguna.net>
2023-10-14 23:05:51 +1300

    More assertions on the search results being as expected

--- tests/E2E/EndToEndTest.php
+++ tests/E2E/EndToEndTest.php
@@ -8,18 +8,19 @@ use Firesphere\ElasticSearch\Queries\ElasticQuery;
 use Firesphere\ElasticSearch\Results\SearchResult;
 use SilverStripe\Control\HTTPRequest;
 use SilverStripe\Dev\SapphireTest;
+use SilverStripe\ORM\ArrayList;
 use SilverStripe\ORM\DatabaseAdmin;
+use SilverStripe\ORM\DataObject;
+use SilverStripe\ORM\PaginatedList;
 
 class EndToEndTest extends SapphireTest
 {
     protected function setUp(): void
     {
-        parent::setUp();
         // Build defaults
         $adm = DatabaseAdmin::singleton();
         $adm->setRequest(new HTTPRequest('GET', 'dev/build', ['quiet' => true]));
         $adm->doBuild(true, true, true);
-
     }
 
     public function testSearching()
@@ -32,5 +33,9 @@ class EndToEndTest extends SapphireTest
         $this->assertArrayHasKey('index', $index->getClientQuery());
         $this->assertGreaterThan(0, $results->getTotalItems());
         $this->assertInstanceOf(SearchResult::class, $results);
+        $this->assertInstanceOf(PaginatedList::class, $results->getPaginatedMatches());
+        $this->assertInstanceOf(ArrayList::class, $results->getMatches());
+        $this->assertEquals($results->getTotalItems(), $results->getPaginatedMatches()->getTotalItems());
+        $this->assertInstanceOf(DataObject::class, $results->getMatches()->first());
     }
 }
