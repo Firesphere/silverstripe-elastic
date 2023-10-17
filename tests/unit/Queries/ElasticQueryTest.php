4050c05a256858f2fabb08f556a5bbc657be7efb
Simon Erkelens <github@casa-laguna.net>
2023-10-17 21:33:51 +1300

    Extract code duplication, add term test (#54)

---
+++ tests/unit/Queries/ElasticQueryTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Firesphere\Elasticsearch\Tests;
+
+use Firesphere\ElasticSearch\Queries\ElasticQuery;
+use SilverStripe\Dev\SapphireTest;
+
+class ElasticQueryTest extends SapphireTest
+{
+
+    public function testTerms()
+    {
+        $query = new ElasticQuery();
+        $query->addTerm('Testing', [], 1);
+
+        $this->assertEquals([
+            [
+                'text'   => 'Testing',
+                'fields' => [],
+                'boost'  => 1
+            ]
+        ], $query->getTerms());
+
+        $query->addTerm('Test 2', ['SiteTree.Title'], 2);
+
+        $this->assertEquals([
+            [
+                'text'   => 'Testing',
+                'fields' => [],
+                'boost'  => 1
+            ],
+            [
+                'text'   => 'Test 2',
+                'fields' => ['SiteTree.Title'],
+                'boost'  => 2
+            ]
+        ], $query->getTerms());
+    }
+}
